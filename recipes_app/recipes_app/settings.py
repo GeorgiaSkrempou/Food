88a777238285e0cb61dd0c98aed16361ff9adcac
georgia <georgiaskrempou@gmail.com>
2022-05-22 21:45:04 +0200

    fix: add allowed host

--- recipes_app/recipes_app/settings.py
+++ recipes_app/recipes_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['DJANGO_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['recipes.sbs.lol']
 
 
 # Application definition
