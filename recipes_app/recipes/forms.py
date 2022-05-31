7dacfd19a13db72f0aa058b8bca5f9ca0fb4ad54
Georgia Skrempou <georgiaskrempou@gmail.com>
2022-06-01 00:20:48 +0200

    wip: added custom user model

--- recipes_app/recipes/forms.py
+++ recipes_app/recipes/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+
+from .models import Account
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        model = Account
+        fields = ['username', 'email', 'password1', 'password2']
