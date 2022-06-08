d1db760860f591088ab72e58fdbb2e04219de2d8
Georgia Skrempou <georgiaskrempou@gmail.com>
2022-06-08 19:21:28 +0200

    feat: email is saved lowecase and in login also lowercased

--- recipes_app/user/admin.py
+++ recipes_app/user/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-
 from .models import Account
+
+
+# Register your models here.
 
 
 class AccountAdmin(UserAdmin):
